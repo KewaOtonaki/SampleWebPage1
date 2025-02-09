c9effe95c7cdbfd2f55ed1a1006fe4602d4a79ee
Otonaki Kewa <30595535+owken64@users.noreply.github.com>
2025-02-09 23:59:14 +0900

    update

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function f() {
 
 };
+
+var info = document.getElementById("info-area");
+var html = "<div class='card'>text</div>";
+info.innerHTML = html + html + html;
+
+var news = document.getElementById("news");
+news.innerText = "ここにニュースが表示されます"
